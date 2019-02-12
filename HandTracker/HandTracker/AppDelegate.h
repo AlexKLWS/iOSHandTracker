d37406505025f192629703ddc91e69229d7f8077
Alex Korzh <alexkorzh7@gmail.com>
2019-02-12 22:48:44 +0300

    Set up project

---
+++ HandTracker/HandTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HandTracker
+//
+//  Created by Alex Korzh on 2/12/19.
+//  Copyright © 2019 Alex Korzh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
